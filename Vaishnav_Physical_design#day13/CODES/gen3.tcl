f004f04bae9aaf54551fe9eae0118c87358d9521
NkVaishnav <nkvaishnav1991@gmail.com>
2023-09-26 19:28:51 +0530

    dfg

---
+++ Vaishnav_Physical_design#day13/CODES/gen3.tcl
@@ -0,0 +1,16 @@
+#TO BE RAN IN LC SHELL
+set file [open "a.txt" "r"];
+set list_name {};
+while { [ gets $file line ] != -1 } {
+	set modified_name [file rootname $line];
+	lappend list_name $modified_name;
+}
+close $file;
+echo $list_name;
+foreach var $list_name {
+	set a "${var}.lib";
+	set b "${var}.db";
+	echo $a;
+	read_lib $a -no_warnings;
+	#write_lib $var -f db -out $b;
+}
